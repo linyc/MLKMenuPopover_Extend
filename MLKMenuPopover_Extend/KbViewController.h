ea3e4caf2f5a809feb8df10bfb35b6554960d83b
linyc <linyc57@163.com>
2015-04-12 22:08:51 +0800

    add KbViewController

---
+++ MLKMenuPopover_Extend/KbViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MLKMenuPopover_Extend
+//
+//  Created by CY on 19/1/15.
+//  Copyright (c) 2015年 LINYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PopMenuView.h"
+
+@interface KbViewController : UITableViewController<PopViewTableViewDelegate>
+
+
+@end
+
